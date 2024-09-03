bdb1b8bb8c86b34ad18a4b21a5d2c8444b89187e
Kapil Garg <kapilgarg9358@gmail.com>
2024-09-03 20:11:01 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "./Components/Modal/Modal";
+
+const App = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Modal.css";
+
+export default function Modal() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
+  return (
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        Open
+      </button>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Modal</h2>
+            <p>
+              Modal is a utility component that renders its children in front of
+              a backdrop. This lets you create an element that your users must
+              interact with before continuing in the parent application.
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
